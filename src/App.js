8a48b6048756654d72a7bdf5993becb52314c1be
Sajid Hussain <sajidmcsbwn@gmail.com>
2023-03-15 14:15:42 +0500

    title is updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <Router>
-    
+    <Router>   
       
       <Navbar></Navbar>
     <Routes>
